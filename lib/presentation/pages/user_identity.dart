7d4fcec19bd00e2266d602a7bb98a4ebb69f0bce
João Dal Lago <joaogabrieldallago@hotmail.com>
2025-03-25 23:02:58 -0300

    :sparkles: feat(public): adiciona página UserIdentity com seleção de idade e sexo
    Este commit introduz a página `UserIdentity`, que permite que os usuários selecionem sua idade e gênero. Ele utiliza o pacote `flutter_riverpod` para gerenciar os estados. A idade é escolhida usando um controle deslizante, e o gênero é selecionado usando botões de opção.

---
+++ lib/presentation/pages/user_identity.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:nina_tito_magic_book/data/models/gender_enum.dart';
+
+final ageProvider = StateProvider<int>((ref) => 9);
+final genderProvider = StateProvider<GenderEnum>((ref) => GenderEnum.male);
+
+class UserIdentity extends ConsumerWidget {
+  const UserIdentity({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final int selectedAge = ref.watch(ageProvider);
+    final GenderEnum selectedGender = ref.watch(genderProvider);
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Quantos anos você tem?',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20),
+
+            Slider(
+              value: selectedAge.toDouble(),
+              min: 6,
+              max: 12,
+              divisions: 6,
+              label: selectedAge.toString(),
+              onChanged: (double value) {
+                ref.read(ageProvider.notifier).state = value.toInt();
+              },
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Row(
+                  children: [
+                    Radio<GenderEnum>(
+                      value: GenderEnum.male,
+                      groupValue: selectedGender,
+                      onChanged: (GenderEnum? value) {
+                        ref.read(genderProvider.notifier).state = value!;
+                      },
+                    ),
+                    Text(selectedGender.label),
+                  ],
+                ),
+
+                Row(
+                  children: [
+                    Radio<GenderEnum>(
+                      value: GenderEnum.female,
+                      groupValue: selectedGender,
+                      onChanged: (GenderEnum? value) {
+                        ref.read(genderProvider.notifier).state = value!;
+                      },
+                    ),
+                    Text(selectedGender.label),
+                  ],
+                ),
+              ],
+            ),
+
+            Text(
+              'Eu sou ${selectedGender.label} e tenho $selectedAge anos!',
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
