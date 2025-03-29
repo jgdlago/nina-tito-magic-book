839cef25b63678aa944756ed6d8aa591e8282373
João Dal Lago <joaogabrieldallago@hotmail.com>
2025-03-29 15:09:45 -0300

    :recycle: refactor(public): ajuste layout pagina de identidade do usuário

--- lib/presentation/pages/user_identity.dart
+++ lib/presentation/pages/user_identity.dart
@@ -29,81 +29,90 @@ class UserIdentity extends ConsumerWidget {
             ),
           ),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
             children: <Widget>[
               Text(
                 'Vamos nos conhecer!',
                 style: Theme.of(context).textTheme.titleMedium,
               ),
-              Text(
-                'Qual sua idade?',
-                style: Theme.of(context).textTheme.bodySmall,
-              ),
               const SizedBox(height: 20),
-              NumberPicker(
-                value: selectedAge,
-                minValue: 4,
-                maxValue: 14,
-                step: 1,
-                axis: Axis.horizontal,
-                selectedTextStyle: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                  fontWeight: FontWeight.bold,
+              Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      'Qual sua idade?',
+                      style: Theme.of(context).textTheme.bodySmall,
+                    ),
+                    const SizedBox(height: 20),
+                    NumberPicker(
+                      value: selectedAge,
+                      minValue: 4,
+                      maxValue: 14,
+                      step: 1,
+                      axis: Axis.horizontal,
+                      selectedTextStyle: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textStyle: const TextStyle(fontSize: 16, color: Colors.blueGrey),
+                      onChanged: (value) {
+                        ref.read(ageProvider.notifier).state = value;
+                      },
+                    ),
+                    Text(
+                      'Você é:',
+                      style: Theme.of(context).textTheme.bodySmall,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Row(
+                          children: [
+                            Radio<GenderEnum>(
+                              value: GenderEnum.male,
+                              groupValue: selectedGender,
+                              activeColor: AppColors.enchantedBlue,
+                              onChanged: (GenderEnum? value) {
+                                ref.read(genderProvider.notifier).state = value!;
+                              },
+                            ),
+                            Text(
+                              GenderEnum.male.label,
+                              style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                                fontWeight: selectedGender == GenderEnum.male ? FontWeight.bold : FontWeight.normal,
+                                color: selectedGender == GenderEnum.male ? AppColors.enchantedBlue : AppColors.mysticalBlack,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Radio<GenderEnum>(
+                              value: GenderEnum.female,
+                              groupValue: selectedGender,
+                              activeColor: AppColors.dreamyPink,
+                              onChanged: (GenderEnum? value) {
+                                ref.read(genderProvider.notifier).state = value!;
+                              },
+                            ),
+                            Text(
+                              GenderEnum.female.label,
+                              style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                                fontWeight: selectedGender == GenderEnum.female ? FontWeight.bold : FontWeight.normal,
+                                color: selectedGender == GenderEnum.female ? AppColors.dreamyPink : AppColors.mysticalBlack,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Text(
+                      'Eu sou ${selectedGender.label} e tenho $selectedAge anos!',
+                      style: Theme.of(context).textTheme.bodySmall,
+                    ),
+                  ],
                 ),
-                textStyle: const TextStyle(fontSize: 16, color: Colors.blueGrey),
-                onChanged: (value) {
-                  ref.read(ageProvider.notifier).state = value;
-                },
-              ),
-              Text(
-                'Você é:',
-                style: Theme.of(context).textTheme.bodySmall,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Row(
-                    children: [
-                      Radio<GenderEnum>(
-                        value: GenderEnum.male,
-                        groupValue: selectedGender,
-                        activeColor: AppColors.enchantedBlue,
-                        onChanged: (GenderEnum? value) {
-                          ref.read(genderProvider.notifier).state = value!;
-                        },
-                      ),
-                      Text(
-                        GenderEnum.male.label,
-                        style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                          fontWeight: selectedGender == GenderEnum.male ? FontWeight.bold : FontWeight.normal,
-                          color: selectedGender == GenderEnum.male ? AppColors.enchantedBlue : AppColors.mysticalBlack,
-                        ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Radio<GenderEnum>(
-                        value: GenderEnum.female,
-                        groupValue: selectedGender,
-                        activeColor: AppColors.dreamyPink,
-                        onChanged: (GenderEnum? value) {
-                          ref.read(genderProvider.notifier).state = value!;
-                        },
-                      ),
-                      Text(
-                        GenderEnum.female.label,
-                        style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                          fontWeight: selectedGender == GenderEnum.female ? FontWeight.bold : FontWeight.normal,
-                          color: selectedGender == GenderEnum.female ? AppColors.dreamyPink : AppColors.mysticalBlack,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              Text(
-                'Eu sou ${selectedGender.label} e tenho $selectedAge anos!',
-                style: Theme.of(context).textTheme.bodySmall,
               ),
             ],
           ),
