f09ac27e0e6ba635134afd6408d6e0552c853799
João Dal Lago <joaogabrieldallago@hotmail.com>
2025-03-29 01:26:45 -0300

    :sparkles: feat(public): cria paleta de cores para o app
    Cria o arquivo `app_colors.dart` e adiciona a paleta de cores do app.

---
+++ lib/presentation/theme/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Cor Principal | Douraodo
+  static const Color goldenMagic = Color(0xFFF5C67C);
+
+  // Contraste
+  static const Color enchantedBlue = Color(0xFF78C2F5); // Azul
+  static const Color dreamyPink = Color(0xFFF78DBF); // Rosa
+  static const Color fantasyGreen = Color(0xFF80E0A7); // Verde
+  static const Color mysteryPurple = Color(0xFFA27BEF); // Roxo
+  static const Color mysticalBlack = Color(0xFF2D2D2D); // Preto
+}
