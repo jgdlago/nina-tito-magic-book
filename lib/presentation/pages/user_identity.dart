365e2c1e6b975c16772630584d5e31fc207ee898
João Dal Lago <joaogabrieldallago@hotmail.com>
2025-03-30 11:38:49 -0300

    :sparkles: feat(public): adiciona botões de ação e estrutura à página user_identity
    - adiciona melhorias de padrão de layout

--- lib/presentation/pages/user_identity.dart
+++ lib/presentation/pages/user_identity.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:nina_tito_magic_book/data/models/gender_enum.dart';
 import 'package:nina_tito_magic_book/presentation/components/background_container.dart';
+import 'package:nina_tito_magic_book/presentation/components/action_button.dart';
+import 'package:nina_tito_magic_book/presentation/pages/main_menu.dart';
 import 'package:nina_tito_magic_book/presentation/theme/app_colors.dart';
 import 'package:numberpicker/numberpicker.dart';
 
@@ -15,106 +17,144 @@ class UserIdentity extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final int selectedAge = ref.watch(ageProvider);
     final GenderEnum selectedGender = ref.watch(genderProvider);
+    final double buttonSize = MediaQuery.of(context).size.width * 0.1;
 
     return Scaffold(
       body: BackgroundContainer(
-        child: Container(
-          padding: const EdgeInsets.all(16),
-          decoration: BoxDecoration(
-            color: AppColors.mysticalWhite,
-            borderRadius: BorderRadius.circular(16),
-            border: Border.all(
-              color: AppColors.mysticalBlack,
-              width: 4,
-            ),
-          ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Text(
-                'Vamos nos conhecer!',
-                style: Theme.of(context).textTheme.titleMedium,
-              ),
-              const SizedBox(height: 20),
-              Center(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      'Qual sua idade?',
-                      style: Theme.of(context).textTheme.bodySmall,
-                    ),
-                    const SizedBox(height: 20),
-                    NumberPicker(
-                      value: selectedAge,
-                      minValue: 4,
-                      maxValue: 14,
-                      step: 1,
-                      axis: Axis.horizontal,
-                      selectedTextStyle: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                        fontWeight: FontWeight.bold,
+        child: Center(
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: Row(
+              children: [
+                SizedBox(
+                  width: buttonSize,
+                  child: ActionButton(
+                    type: ButtonType.denial,
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => MainMenu()),
+                      );
+                    },
+                  ),
+                ),
+                const SizedBox(width: 16),
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      color: AppColors.mysticalWhite,
+                      borderRadius: BorderRadius.circular(16),
+                      border: Border.all(
+                        color: AppColors.mysticalBlack,
+                        width: 4,
                       ),
-                      textStyle: const TextStyle(fontSize: 16, color: Colors.blueGrey),
-                      onChanged: (value) {
-                        ref.read(ageProvider.notifier).state = value;
-                      },
                     ),
-                    Text(
-                      'Você é:',
-                      style: Theme.of(context).textTheme.bodySmall,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
                       children: <Widget>[
-                        Row(
-                          children: [
-                            Radio<GenderEnum>(
-                              value: GenderEnum.male,
-                              groupValue: selectedGender,
-                              activeColor: AppColors.enchantedBlue,
-                              onChanged: (GenderEnum? value) {
-                                ref.read(genderProvider.notifier).state = value!;
-                              },
-                            ),
-                            Text(
-                              GenderEnum.male.label,
-                              style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                                fontWeight: selectedGender == GenderEnum.male ? FontWeight.bold : FontWeight.normal,
-                                color: selectedGender == GenderEnum.male ? AppColors.enchantedBlue : AppColors.mysticalBlack,
-                              ),
-                            ),
-                          ],
+                        Text(
+                          'Vamos nos conhecer!',
+                          style: Theme.of(context).textTheme.titleMedium,
                         ),
-                        Row(
-                          children: [
-                            Radio<GenderEnum>(
-                              value: GenderEnum.female,
-                              groupValue: selectedGender,
-                              activeColor: AppColors.dreamyPink,
-                              onChanged: (GenderEnum? value) {
-                                ref.read(genderProvider.notifier).state = value!;
-                              },
-                            ),
-                            Text(
-                              GenderEnum.female.label,
-                              style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                                fontWeight: selectedGender == GenderEnum.female ? FontWeight.bold : FontWeight.normal,
-                                color: selectedGender == GenderEnum.female ? AppColors.dreamyPink : AppColors.mysticalBlack,
+                        const SizedBox(height: 20),
+                        Center(
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text(
+                                'Qual sua idade?',
+                                style: Theme.of(context).textTheme.bodySmall,
                               ),
-                            ),
-                          ],
+                              const SizedBox(height: 20),
+                              NumberPicker(
+                                value: selectedAge,
+                                minValue: 4,
+                                maxValue: 14,
+                                step: 1,
+                                axis: Axis.horizontal,
+                                selectedTextStyle: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                                  fontWeight: FontWeight.bold,
+                                ),
+                                textStyle: const TextStyle(fontSize: 16, color: Colors.blueGrey),
+                                onChanged: (value) {
+                                  ref.read(ageProvider.notifier).state = value;
+                                },
+                              ),
+                              Text(
+                                'Você é:',
+                                style: Theme.of(context).textTheme.bodySmall,
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Row(
+                                    children: [
+                                      Radio<GenderEnum>(
+                                        value: GenderEnum.male,
+                                        groupValue: selectedGender,
+                                        activeColor: AppColors.enchantedBlue,
+                                        onChanged: (GenderEnum? value) {
+                                          ref.read(genderProvider.notifier).state = value!;
+                                        },
+                                      ),
+                                      Text(
+                                        GenderEnum.male.label,
+                                        style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                                          fontWeight: selectedGender == GenderEnum.male ? FontWeight.bold : FontWeight.normal,
+                                          color: selectedGender == GenderEnum.male ? AppColors.enchantedBlue : AppColors.mysticalBlack,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  Row(
+                                    children: [
+                                      Radio<GenderEnum>(
+                                        value: GenderEnum.female,
+                                        groupValue: selectedGender,
+                                        activeColor: AppColors.dreamyPink,
+                                        onChanged: (GenderEnum? value) {
+                                          ref.read(genderProvider.notifier).state = value!;
+                                        },
+                                      ),
+                                      Text(
+                                        GenderEnum.female.label,
+                                        style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                                          fontWeight: selectedGender == GenderEnum.female ? FontWeight.bold : FontWeight.normal,
+                                          color: selectedGender == GenderEnum.female ? AppColors.dreamyPink : AppColors.mysticalBlack,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                              Text(
+                                'Eu sou ${selectedGender.label} e tenho $selectedAge anos!',
+                                style: Theme.of(context).textTheme.bodySmall,
+                              ),
+                            ],
+                          ),
                         ),
                       ],
                     ),
-                    Text(
-                      'Eu sou ${selectedGender.label} e tenho $selectedAge anos!',
-                      style: Theme.of(context).textTheme.bodySmall,
-                    ),
-                  ],
+                  ),
                 ),
-              ),
-            ],
+                const SizedBox(width: 16),
+                SizedBox(
+                  width: buttonSize,
+                  child: ActionButton(
+                    type: ButtonType.confirmation,
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => MainMenu()),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
