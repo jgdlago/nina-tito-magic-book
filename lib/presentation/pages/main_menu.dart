5ad60cdd4d761fb3c6c9c69323f7fee81517b79c
João Dal Lago <joaogabrieldallago@hotmail.com>
2025-03-30 16:25:14 -0300

    :recycle: refactor(public): melhoria nos padrões de código do menu principal

--- lib/presentation/pages/main_menu.dart
+++ lib/presentation/pages/main_menu.dart
@@ -8,101 +8,128 @@ class MainMenu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final double buttonSize = MediaQuery.of(context).size.width * 0.15;
-
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/images/background.png"),
-            fit: BoxFit.cover,
-            colorFilter: ColorFilter.mode(
-              Color(0x60000000),
-              BlendMode.darken,
+      body: Stack(
+        children: const [
+          _Background(),
+          SafeArea(
+            child: Column(
+              children: [
+                SizedBox(height: 50),
+                _TitleSection(),
+                Spacer(),
+                _BottomButtons(),
+              ],
             ),
           ),
-        ),
-        child: Stack(
-          children: [
-            Align(
-              alignment: Alignment.topCenter,
-              child: Padding(
-                padding: const EdgeInsets.only(top: 50.0),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(
-                      'As aventuras de Nina e Tito',
-                      style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                        color: AppColors.mysticalWhite,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                    const SizedBox(height: 8),
-                    Text(
-                      'Em busca do livro mágico',
-                      style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                        color: AppColors.mysticalWhite,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Align(
-              alignment: Alignment.bottomLeft,
-              child: Padding(
-                padding: const EdgeInsets.only(left: 50.0, bottom: 50.0),
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.075,
-                  child: CustomIconButton(
-                    type: IconType.info,
-                    onPressed: getSettingsScreen,
-                    color: AppColors.warningAmber,
-                  ),
-                ),
-              ),
-            ),
-            Align(
-              alignment: Alignment.bottomLeft,
-              child: Padding(
-                padding: const EdgeInsets.only(left: 120.0, bottom: 50.0),
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.075,
-                  child: CustomIconButton(
-                    type: IconType.more,
-                    onPressed: getSettingsScreen,
-                    color: AppColors.warningAmber,
-                  ),
-                ),
-              ),
-            ),
-            Align(
-              alignment: Alignment.bottomRight,
-              child: Padding(
-                padding: const EdgeInsets.only(right: 50.0, bottom: 50.0),
-                child: SizedBox(
-                  width: buttonSize,
-                  child: CustomIconButton(
-                    type: IconType.play,
-                    onPressed: () => getGameScreen(context),
-                    color: AppColors.confirmationGreen,
-                  ),
-                ),
-              ),
-            ),
-          ],
+        ],
+      ),
+    );
+  }
+}
+
+class _Background extends StatelessWidget {
+  const _Background();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("assets/images/background.png"),
+          fit: BoxFit.cover,
+          colorFilter: ColorFilter.mode(
+            Color(0x60000000),
+            BlendMode.darken,
+          ),
         ),
       ),
     );
   }
+}
 
-  Widget getSettingsScreen() {
-    return MainMenu();
+class _TitleSection extends StatelessWidget {
+  const _TitleSection();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(
+          'As aventuras de Nina e Tito',
+          style: Theme.of(context).textTheme.titleLarge!.copyWith(
+            color: AppColors.mysticalWhite,
+          ),
+          textAlign: TextAlign.center,
+        ),
+        const SizedBox(height: 8),
+        Text(
+          'Em busca do livro mágico',
+          style: Theme.of(context).textTheme.titleMedium!.copyWith(
+            color: AppColors.mysticalWhite,
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ],
+    );
+  }
+}
+
+class _BottomButtons extends StatelessWidget {
+  const _BottomButtons();
+
+  @override
+  Widget build(BuildContext context) {
+    final double iconButtonSize = MediaQuery.of(context).size.width * 0.075;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 50),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              SizedBox(
+                width: iconButtonSize,
+                child: CustomIconButton(
+                  type: IconType.info,
+                  onPressed: () => _openSettings(context),
+                  color: AppColors.warningAmber,
+                ),
+              ),
+              const SizedBox(width: 20),
+              SizedBox(
+                width: iconButtonSize,
+                child: CustomIconButton(
+                  type: IconType.more,
+                  onPressed: () => _openSettings(context),
+                  color: AppColors.warningAmber,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 0.15,
+            child: CustomIconButton(
+              type: IconType.play,
+              onPressed: () => _openGameScreen(context),
+              color: AppColors.confirmationGreen,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
-  void getGameScreen(BuildContext context) {
+  void _openSettings(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const MainMenu()),
+    );
+  }
+
+  void _openGameScreen(BuildContext context) {
     Navigator.push(
       context,
       MaterialPageRoute(builder: (context) => const UserIdentity()),
