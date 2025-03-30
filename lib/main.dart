5dd5816241534c9ca095f69d474ce8414fe6fef6
João Dal Lago <joaogabrieldallago@hotmail.com>
2025-03-30 18:42:16 -0300

    :sparkles: feat(public): adiciona ProviderScope e tema do app

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,31 @@
 import 'package:flame/flame.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:nina_tito_magic_book/data/datasources/database_helper.dart';
-import 'package:nina_tito_magic_book/game/magic_book_game.dart';
 import 'presentation/pages/main_menu.dart';
+import 'presentation/theme/theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await _initializeApp();
+  runApp(const ProviderScope(child: MyApp()));
+}
 
+Future<void> _initializeApp() async {
   await DatabaseHelper().database;
   await Flame.device.fullScreen();
   await Flame.device.setLandscape();
-
-  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MainMenu(
-        onStartGame: () {
-          runApp(const MagicBookGameWidget());
-        },
-        onSettings: () {
-          debugPrint('Abrir Configurações');
-        },
-      ),
+      theme: AppTheme.theme,
+      home: const MainMenu(),
     );
   }
 }
