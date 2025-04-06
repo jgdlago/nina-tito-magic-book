53fe2f9bb6ad628d863afeb393634d4312e837dc
João Dal Lago <joaogabrieldallago@hotmail.com>
2025-04-06 11:06:04 -0300

    :sparkles: feat(public): adiciona botão para sair da tela de informações
    - Adiciona um botão para sair da tela de informações
    - Ajusta layout da tela de informações

--- lib/presentation/pages/info.dart
+++ lib/presentation/pages/info.dart
@@ -1,33 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:nina_tito_magic_book/presentation/components/action_button.dart';
 import 'package:nina_tito_magic_book/presentation/components/background_container.dart';
 import 'package:nina_tito_magic_book/presentation/components/Info_modal.dart';
+import 'package:nina_tito_magic_book/presentation/components/custom_icon_button.dart';
+import 'package:nina_tito_magic_book/presentation/theme/app_colors.dart';
 
 class Info extends StatelessWidget {
   const Info({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final double buttonSize = MediaQuery.of(context).size.width * 0.15;
     final double modalWidth = MediaQuery.of(context).size.width * 0.6;
 
     return Scaffold(
       body: BackgroundContainer(
         child: Center(
-          child: InfoModal(
-            width: modalWidth,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                Text(
-                  'Bem Vindo!',
-                  style: Theme.of(context).textTheme.titleMedium,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              InfoModal(
+                width: modalWidth,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Text(
+                      'Bem Vindo!',
+                      style: Theme.of(context).textTheme.titleMedium,
+                    ),
+                    Text(
+                      'Nina e Tito: Em busca do livro mágico é um jogo educativo voltado para crianças de 6 a 12 anos. Seu principal objetivo é ajudar na prevenção, identificação e conscientização sobre possíveis situações de vulnerabilidade infantil. O conteúdo do jogo é baseado em uma pesquisa desenvolvida pela psicóloga Me. Jussara Besutti.',
+                      style: Theme.of(context).textTheme.bodySmall,
+                    ),
+                  ],
                 ),
-                Text(
-                  'Nina e Tito: Em busca do livro mágico é um jogo educativo voltado para crianças de 6 a 12 anos. Seu principal objetivo é ajudar na prevenção, identificação e conscientização sobre possíveis situações de vulnerabilidade infantil. O conteúdo do jogo é baseado em uma pesquisa desenvolvida pela psicóloga Me. Jussara Besutti.',
-                  style: Theme.of(context).textTheme.bodySmall,
+              ),
+              const SizedBox(height: 16),
+              SizedBox(
+                width: buttonSize,
+                child: ActionButton(
+                  type: ButtonType.denial,
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
                 ),
-              ],
-            ),
-          )
+              ),
+            ],
+          ),
         ),
       ),
     );
