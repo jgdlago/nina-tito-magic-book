0138b961f1041879ec885d2c99daddfb73db01c0
João Dal Lago <joaogabrieldallago@hotmail.com>
2025-07-06 17:52:34 -0300

    :construction: construction(game): adiciona mapa da escola para nível 2

---
+++ assets/tiles/school_tiled.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1-99-gec89c545" name="school_tiled" tilewidth="64" tileheight="64" tilecount="1922" columns="62">
+ <image source="../images/school_tiled_64x64.png" width="4000" height="2000"/>
+</tileset>
