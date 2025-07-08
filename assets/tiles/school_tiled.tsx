ac474527fd8653b069eda0995ea0c687cb2ac417
João Dal Lago <joaogabrieldallago@hotmail.com>
2025-07-08 01:02:49 -0300

    :art: feat(game): atualiza mapa da escola para versão menor

--- assets/tiles/school_tiled.tsx
+++ assets/tiles/school_tiled.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.1-99-gec89c545" name="school_tiled" tilewidth="64" tileheight="64" tilecount="1922" columns="62">
- <image source="../images/school_tiled_64x64.png" width="4000" height="2000"/>
+<tileset version="1.10" tiledversion="1.11.1-99-gec89c545" name="school_tiled_64x64" tilewidth="64" tileheight="64" tilecount="242" columns="22">
+ <image source="../images/school_tiled_64x64.png" width="1408" height="704"/>
 </tileset>
